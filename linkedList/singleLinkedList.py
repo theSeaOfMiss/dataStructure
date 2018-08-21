bbfe47d1e88f7aa8e0bca9452b4c59dbacdb301c
zhangxiang <xnzh@foxmail.com>
2018-08-21 16:57:08 +0800

    Minor change

--- linkedList/singleLinkedList.py
+++ linkedList/singleLinkedList.py
@@ -13,8 +13,8 @@ class Student:
 
 class LinkList:
     def __init__(self):
-        self.head = None
-        self.length = 0
+        self.__head = None
+        self.__length = 0
 
     # 向链表尾部添加节点
     def append(self, node):
@@ -23,19 +23,19 @@ class LinkList:
             return
 
         node.next = None
-        if not self.head:
-            self.head = node
+        if not self.__head:
+            self.__head = node
         else:
-            temp = self.head
+            temp = self.__head
             while temp.next:
                 temp = temp.next
             temp.next = node
-        self.length += 1
+        self.__length += 1
 
     # 展示链表中所有节点
     def display(self, attr):
-        if self.length:
-            temp = self.head
+        if self.__length:
+            temp = self.__head
             try:
                 print(getattr(temp, attr))
             except ValueError as e:
@@ -51,19 +51,19 @@ class LinkList:
 
     # 判断是否为空,若不为空，则在控制台显示链表中有多少节点
     def is_empty(self):
-        if self.length:
-            print('节点数为：', self.length)
+        if self.__length:
+            # print('节点数为：', self.__length)
             return False
         else:
-            print('此链表为空链表！')
+            # print('此链表为空链表！')
             return True
 
     # 通过索引获得相应节点，head的索引为1
     def get_node_by_index(self, index):
-        if index > self.length or index <= 0:
+        if index > self.__length or index <= 0:
             print('索引超出范围！')
         else:
-            temp = self.head
+            temp = self.__head
             for i in range(index-1):
                 temp = temp.next
             return temp
@@ -74,37 +74,37 @@ class LinkList:
             print('链表中已包含此数据，请勿重复添加')
             return
 
-        if index > self.length or index <= 0:
+        if index > self.__length or index <= 0:
             print('索引超出范围！')
         else:
-            temp = self.head
+            temp = self.__head
             if index == 1:
                 inter = temp        # 中间变量
-                self.head = node
-                self.head.next = inter
+                self.__head = node
+                self.__head.next = inter
             else:
                 for i in range(index - 2):
                     temp = temp.next
                 inter = temp.next       # 中间变量
                 temp.next = node
                 node.next = inter
-            self.length += 1
+            self.__length += 1
 
     # 删除索引处的节点
     def delete(self, index):
-        if index > self.length or index <= 0:
+        if index > self.__length or index <= 0:
             print('索引超出范围！')
         else:
-            temp = self.head
+            temp = self.__head
             if index == 1:
                 inter = temp.next
-                self.head = inter
+                self.__head = inter
             else:
                 for i in range(index - 2):
                     temp = temp.next
                 inter = temp.next       # 将需要去掉的节点赋值给inter
                 temp.next = inter.next
-            self.length -= 1
+            self.__length -= 1
             print('成功删除节点%d' % index)
 
     # 更新索引处的节点
@@ -113,13 +113,13 @@ class LinkList:
             print('链表中已包含此数据，请勿重复添加')
             return
 
-        if index > self.length or index <= 0:
+        if index > self.__length or index <= 0:
             print('索引超出范围！')
         else:
-            temp = self.head
+            temp = self.__head
             if index == 1:
                 inter = temp.next
-                self.head = node
+                self.__head = node
                 node.next = inter
             else:
                 for i in range(index - 2):
@@ -131,8 +131,8 @@ class LinkList:
 
     # 判断节点是否在链表中存在, 如果存在返回True
     def is_exist(self, node):
-        temp = self.head
-        for i in range(self.length):
+        temp = self.__head
+        for i in range(self.__length):
             if node == temp:
                 return True
             temp = temp.next
@@ -140,11 +140,11 @@ class LinkList:
 
     # 清空链表
     def clear(self):
-        self.head = None
-        self.length = 0
+        self.__head = None
+        self.__length = 0
         print('已经清空链表！')
 
     # 返回链表长度
     def get_length(self):
-        return self.length
+        return self.__length
 
